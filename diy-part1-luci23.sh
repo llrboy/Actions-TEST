59d2ad7eb403c32a2cdd834b2e03701044302123
nhhqgirl <121777332+nhhqgirl@users.noreply.github.com>
2026-04-11 22:40:12 +0800

    Update diy-part1-luci23.sh

--- diy-part1-luci23.sh
+++ diy-part1-luci23.sh
@@ -10,46 +10,15 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-
-# Add a feed source
-# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
-# svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone --depth=1 https://github.com/fw876/helloworld
-
-
-# Add Nikki(MiHomo)
-# echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
-
-
-# Add DDNSTO
-echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
-echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
-
-
-# Add cpufreq
-# rm -rf ./feeds/luci/applications/luci-app-cpufreq
-# svn co https://github.com/DHDAXCW/luci-bt/trunk/applications/luci-app-cpufreq ./feeds/luci/applications/luci-app-cpufreq
-# ln -sf ./feeds/luci/applications/luci-app-cpufreq ./package/feeds/luci/luci-app-cpufreq
-# sed -i 's,1608,1800,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/10-cpufreq
-# sed -i 's,2016,2208,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/10-cpufreq
-# sed -i 's,1512,1608,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/10-cpufreq
-
 # Add OpenClash
 git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
-# Add Lucky
-git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
+
 
 # Add luci-app-bandix
 git clone https://github.com/timsaya/openwrt-bandix.git package/bandix
 git clone https://github.com/timsaya/luci-app-bandix.git package/luci-app-bandix
 
-# Add luci-app-wechatpush
-git clone https://github.com/tty228/luci-app-wechatpush.git package/luci-app-wechatpush
-
-# Add OpenAppFilter
-# git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+# 添加插件
+git clone https://github.com/nhhqgirl/luci-app-onliner.git package/lean/luci-app-onliner
+git clone --depth=1 -b master https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
